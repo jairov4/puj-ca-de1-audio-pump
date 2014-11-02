8804c7b2c951379e1e15ee17d8a0082e101c91aa
JairoAndres <JairoAndres@Razor>
2014-11-02 14:09:49 -0500

    New IP for AvlMM passthrough

---
+++ system_design/avl_mm_master_export.v
@@ -0,0 +1,7 @@
+module avl_mm_master_export
+(clk, reset,
+avm_readdata, avm_writedata, avm_read, avm_write, avm_address, avm_waitrequest,
+e_readdata, e_writedata, e_read, e_write, e_address, e_waitrequest, clk, reset);
+
+
+endmodule;
